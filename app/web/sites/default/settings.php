65e56f415f0b28538c174344738f4a02fa7a07af
local <local>
2022-04-02 17:25:43 +0530

    Drupal Docket Based dsetup

---
+++ app/web/sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal_database_user',
+  'password' => 'drupal_database_password',
+  'prefix' => '',
+  'host' => 'mysql',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+// $settings['config_sync_directory'] = 'sites/default/files/config_Dl5zDMsClRXf75LsHNN8iHRWXBvKwM-svgpwJX77rsQ7Wm2IsBlP3Cc1tg4wBlsw8hKUmxvNhQ/sync';
+$config['system.logging']['error_level'] = 'verbose';
