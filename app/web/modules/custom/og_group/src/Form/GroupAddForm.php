132d9b2232ef5829ce048de368a8433a5c9e98e9
local <local>
2022-04-03 10:31:36 +0530

    Logic to get the Group entity fields in custom form

--- app/web/modules/custom/og_group/src/Form/GroupAddForm.php
+++ app/web/modules/custom/og_group/src/Form/GroupAddForm.php
@@ -3,17 +3,63 @@ namespace Drupal\og_group\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityFieldManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class GroupAddForm extends FormBase {
+
+  protected $entity_type_manager;
+  protected $entityFieldManager;
+
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, EntityFieldManager $entityFieldManager) {
+    $this->entity_type_manager = $entity_type_manager;
+    $this->entityFieldManager = $entityFieldManager;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager')
+    );
+  }
+
 
   public function getFormId() {
     return 'og_group_add';
   }
 
   public function buildForm(array $form, FormStateInterface $form_state){
-    $form['title'] = [
-      'type' => 'textfield',
+
+    // $og_group = $this->entity_type_manager->getStorage('og_group');
+
+    $og_group = $this->entity_type_manager->getStorage('node_type')->load('og_group');
+
+    $fields = $this->entityFieldManager->getFieldDefinitions('node', 'og_group');
+
+    // print_r($fields);
+
+    $listFields = [];
+    foreach ($fields as $field_name => $field_definition) {
+      if (!empty($field_definition->getTargetBundle())) {
+        $listFields[$field_name]['type'] = $field_definition->getType();
+        $listFields[$field_name]['label'] = $field_definition->getLabel();
+      }
+    }
+    foreach ($listFields as $field_name => $info) {
+      $form[$field_name] = [
+        '#type' => $info['type'],
+        '#title' => $info['label'],
+      ];
+    }
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add'),
+      '#button_type' => 'primary',
     ];
+    return $form;
+
   }
 
   public function submitForm(&$form, FormStateInterface $form_state){
